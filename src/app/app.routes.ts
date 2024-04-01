0dc77464d109f988c932f0724932b2ca0a8a38fa
Miles <87283172+MilesPurvis@users.noreply.github.com>
2024-04-01 15:38:18 -0400

    Fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {AddpageComponent} from "./components/addpage/addpage.component";
+import {SettingspageComponent} from "./components/settingspage/settingspage.component";
+import {DetailpageComponent} from "./components/detailpage/detailpage.component";
+import {ShowpageComponent} from "./components/showpage/showpage.component";
+import {HomepageComponent} from "./components/homepage/homepage.component";
+import {ErrorpageComponent} from "./components/errorpage/errorpage.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "home", component: HomepageComponent},
+  {path: "show", component: ShowpageComponent},
+  {path: "detail/:id", component: DetailpageComponent},
+  {path: "settings", component: SettingspageComponent},
+  {path: "add", component: AddpageComponent},
+  {path: "", redirectTo: "home",pathMatch:"full"},
+  {path: "**", component: ErrorpageComponent},
+];
