b93adf738ef34e28c29a67df57a6710403135b8c
Miles <87283172+MilesPurvis@users.noreply.github.com>
2024-04-04 12:35:01 -0400

    navbar update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,31 @@
 import { Routes } from '@angular/router';
-import {AddpageComponent} from "./components/addpage/addpage.component";
 import {SettingspageComponent} from "./components/settingspage/settingspage.component";
 import {DetailpageComponent} from "./components/detailpage/detailpage.component";
-import {ShowpageComponent} from "./components/showpage/showpage.component";
 import {HomepageComponent} from "./components/homepage/homepage.component";
 import {ErrorpageComponent} from "./components/errorpage/errorpage.component";
+import {ShowDronesComponent} from "./components/show-drones/show-drones.component";
+import {ShowGroupsComponent} from "./components/show-groups/show-groups.component";
+import {ShowZonesComponent} from "./components/show-zones/show-zones.component";
+import {AddDronesComponent} from "./components/add-drones/add-drones.component";
+import {AddGroupsComponent} from "./components/add-groups/add-groups.component";
+import {AddZonesComponent} from "./components/add-zones/add-zones.component";
 
 export const routes: Routes = [
   {path: "home", component: HomepageComponent},
-  {path: "show", component: ShowpageComponent},
+
+  //show
+  {path: "showDrones", component: ShowDronesComponent},
+  {path: "showGroups", component: ShowGroupsComponent},
+  {path: "showZones", component: ShowZonesComponent},
+
+  //add
+  {path: "addDrones", component: AddDronesComponent},
+  {path: "addGroups", component: AddGroupsComponent},
+  {path: "addZones", component: AddZonesComponent},
+
+  //Edit
   {path: "detail/:id", component: DetailpageComponent},
   {path: "settings", component: SettingspageComponent},
-  {path: "add", component: AddpageComponent},
   {path: "", redirectTo: "home",pathMatch:"full"},
   {path: "**", component: ErrorpageComponent},
 ];
---
+++ src/app/components/add-drones/add-drones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-drones',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-drones.component.html',
+  styleUrl: './add-drones.component.css'
+})
+export class AddDronesComponent {
+
+}
