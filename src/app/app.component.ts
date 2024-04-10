3f8564a06aa77141006b92a5efba6c0e5ad3ac03
Stefan <103537403+Stefan-Kob@users.noreply.github.com>
2024-04-10 12:11:43 -0400

    Edited the add drones page, nav page, and added camera service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavComponent} from "./components/nav/nav.component";
 import {FooterComponent} from "./footer/footer.component";
+import {CameraComponent} from "./components/camera/camera.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, FooterComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent, CameraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
