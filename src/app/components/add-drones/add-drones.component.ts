cff2f1342a7515cfb9c7844630982ac551730bc9
Stefan <103537403+Stefan-Kob@users.noreply.github.com>
2024-04-10 22:22:43 -0400

    Fixed the add drone form, only the camera remains

--- src/app/components/add-drones/add-drones.component.ts
+++ src/app/components/add-drones/add-drones.component.ts
@@ -2,7 +2,7 @@ import {Component, inject} from '@angular/core';
 import {CameraComponent} from "../camera/camera.component";
 import {Drone} from "../../models/drone.model";
 import {DroneDalService} from "../../services/drone-dal.service";
-import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-add-drones',
@@ -15,11 +15,12 @@ import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angula
   templateUrl: './add-drones.component.html',
   styleUrl: './add-drones.component.css'
 })
+
+
 export class AddDronesComponent {
-  drone:Drone = new Drone("Dji", "Mini 2 SE", "El Drono", "white", 249, false, false, null);
   dal = inject(DroneDalService);
   builder = inject(FormBuilder)
-  defaultColorValue = "white"
+  defaultColorValue = "White"
   MIN_WEIGHT = 100;
   MAX_WEIGHT = 50000;
 
@@ -29,14 +30,23 @@ export class AddDronesComponent {
     droneModel: ["", [Validators.required]],
     droneName: ["", [Validators.required]],
     droneWeight: ["", [Validators.required, Validators.min(this.MIN_WEIGHT), Validators.max(this.MAX_WEIGHT)]],
+    pilotCert: [false],
+    customModified: [false],
+    droneColor: ["White"]
   });
 
-
-
-
-
+  drone:Drone = new Drone("", "", "", "White", 0, false, false, null);
 
   onAddClick() {
+    // Update drone object with the retrieved value
+    this.drone.droneBrand = String(this.addDroneForm.get("droneBrand")?.value);
+    this.drone.droneModel = String(this.addDroneForm.get("droneModel")?.value);
+    this.drone.droneName = String(this.addDroneForm.get("droneName")?.value);
+    this.drone.droneColor = String(this.addDroneForm.get("droneColor")?.value);
+    this.drone.droneWeight = Number(this.addDroneForm.get("droneWeight")?.value);
+    this.drone.pilotCert = Boolean(this.addDroneForm.get("pilotCert")?.value);
+    this.drone.customModified = Boolean(this.addDroneForm.get("customModified")?.value);
+
     this.dal.insert(this.drone).then((data) => {
       console.log(data);
       alert("Record added successfully")
