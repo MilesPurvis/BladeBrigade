1d5dca178dc6a46a90b19bc5f8f10f926398df4d
Stefan <103537403+Stefan-Kob@users.noreply.github.com>
2024-04-13 12:52:32 -0400

    Added database functions

--- src/app/components/settingspage/settingspage.component.ts
+++ src/app/components/settingspage/settingspage.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {DatabaseService} from "../../services/database.service";
 
 @Component({
   selector: 'app-settingspage',
@@ -11,5 +12,39 @@ import {RouterLink} from "@angular/router";
   styleUrl: './settingspage.component.css'
 })
 export class SettingspageComponent {
+  database =inject(DatabaseService);
 
+  CreateDBFunction(){
+    this.database.createDatabase().then(data => {
+      console.log("Database Created Successfully")
+    }).catch(e => {
+      console.error(e)
+    });
+  }
+
+  CreateAllTablesDBFunction(){
+    this.CreateDroneTableDBFunction();
+    this.CreateZoneTableDBFunction();
+    this.CreateGroupTableDBFunction();
+  }
+
+  DeleteDBFunction(){
+    this.database.deleteDatabase().then(data => {
+      console.log("Database deleted Successfully")
+    }).catch(e => {
+      console.error(e)
+    });
+  }
+
+  CreateDroneTableDBFunction(){
+    this.database.CreateDroneTable();
+  }
+
+  CreateZoneTableDBFunction() {
+    this.database.CreateZoneTable();
+  }
+
+  CreateGroupTableDBFunction(){
+    this.database.CreateGroupTable();
+  }
 }
